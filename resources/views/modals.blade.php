56768fbc08aae27157d52d0e20e0dfceb17fe3c6
Nerdy Naga <mozhui.lungdsuo@gmail.com>
2026-02-09 14:41:59 +0530

    added new design

--- resources/views/modals.blade.php
+++ resources/views/modals.blade.php
@@ -1,17 +1,15 @@
 <div wire:ignore.self>
-    '<flux:modal name="kezeneilhou-global-modal" class="min-w-[20rem] space-y-6" :width="$this->getFluxWidth()"
+    <flux:modal name="kezeneilhou-global-modal" class="w-full"
         x-on:open-global-modal.window="Flux.modal('kezeneilhou-global-modal').show()"
         x-on:close-global-modal.window="Flux.modal('kezeneilhou-global-modal').hide()"
         x-on:close="$wire.dispatch('resetModal')">
-        <div>
-            <flux:heading size="lg">{{ $title }}</flux:heading>
-        </div>
 
-        {{-- BODY --}}
-        <div class="mt-2">
-            @if ($alias)
-                @livewire($alias, $params, key($activemodal))
-            @endif
-        </div>
+
+        @if ($alias)
+            @livewire($alias, $params, key($activemodal))
+        @endif
+
     </flux:modal>
+
+
 </div>
